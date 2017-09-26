a93ec6e26419dc7bdcb56bc94f64758a82306796
John Jones <jdj5a@users.noreply.github.com>
2017-09-25 23:06:12 -0600

    Update lessonClass.js
    use closure

--- lessonClass.js
+++ lessonClass.js
@@ -1,28 +1,31 @@
 lesson = {};
-lesson.helloLib = ["Hello ", "[name]"];
-lesson.simpleLib = ["Please excuse ", "[person]", " from class because (s)he is ", "[adjective]"];
-lesson.longLib = [
-"Once upon a time, there was a ", "[adjective]", " girl who lived in a ",
-"[noun]", " near the forest.  Whenever she went out, she wore a red riding ",
-"[noun]", ", so everyone in the ", "[noun]", " called her ", "[adjective]",
-" Red Riding ", "[noun]", "."];
+$(function() {
+  $('body').html('<div id="output"></div>');
+  lesson.helloLib = ["Hello ", "[name]"];
+  lesson.simpleLib = ["Please excuse ", "[person]", " from class because (s)he is ", "[adjective]"];
+  lesson.longLib = [
+  "Once upon a time, there was a ", "[adjective]", " girl who lived in a ",
+  "[noun]", " near the forest.  Whenever she went out, she wore a red riding ",
+  "[noun]", ", so everyone in the ", "[noun]", " called her ", "[adjective]",
+  " Red Riding ", "[noun]", "."];
 
-lesson.getHelloLib = function() {
-  return this.helloLib;
-};
-lesson.getSimpleLib = function() {
-  return this.simpleLib;
-};
-lesson.getLongLib = function() {
-  return this.longLib;
-};
+  lesson.getHelloLib = function() {
+    return this.helloLib;
+  };
+  lesson.getSimpleLib = function() {
+    return this.simpleLib;
+  };
+  lesson.getLongLib = function() {
+    return this.longLib;
+  };
 
-lesson.print = function(text) {
-  //alert(text);
-  let output = document.getElementById("output");
-  let content = output.innerHTML;
-  if (content.trim().length > 0) {
-    content += "<br>\n";
-  }
-  output.innerHTML = content + text;
-};
+  lesson.print = function(text) {
+    //alert(text);
+    let output = document.getElementById("output");
+    let content = output.innerHTML;
+    if (content.trim().length > 0) {
+      content += "<br>\n";
+    }
+    output.innerHTML = content + text;
+  };
+});
